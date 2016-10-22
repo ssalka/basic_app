7ffc381deadea0024c701237e39626f73b61b0f1
Steven Salka <steven@ssalka.io>
2016-10-22 09:21:51 -0400

    remove test /count route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -7,11 +7,6 @@ const router = express.Router();
  * GET ROUTES
  */
 
-router.get('/count', (req, res) => {
-  req.session.count = req.session.count + 1 || 0;
-  res.json({ count: req.session.count });
-});
-
 router.get('/me',
   middleware.getUser
 );
