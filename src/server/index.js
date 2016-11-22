6c066b78440ad331b4c0564f84a7110de24228db
Steven Salka <steven@ssalka.io>
2016-11-21 21:53:00 -0500

    talking to database w/ graphql via graphiql

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 const session = require('express-session');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { buildSchema } = require('graphql');
+const _ = require('lodash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const { types: { ObjectId } } = require('lib/server/utils');
 
 const {
   db: { conn },
-  models: { User }
+  models: { User, Collection },
+  utils: { types }
 } = require('lib/server');
+
 const routes = require('./routes');
 const config = require('./config');
 
@@ -26,7 +32,55 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+const schema = buildSchema(`
+  type Query {
+    me: User
+    hello: String
+    user(id: String): User
+    users(limit: Int): [User]
+    collections(id: String, creator: String): [Collection]
+  }
+
+  type User {
+    # A user on the site
+    _id: String
+    username: String
+    email: String
+    library: Library
+  }
+
+  type Library {
+    # A user's content
+    collections: [Collection]
+  }
+
+  type Collection {
+    # A collection of documents owned by a user
+    _id: String
+    name: String
+    creator: User
+  }
+`);
+
+const loaders = {
+  me: () => User.findOne({ _id: "581f60e5a3193e23932cd6eb" }),
+  user: ({id}) => User.findById(id).populate('library.collections').exec(),
+  users: ({ids, limit = 0}) => {
+    const query = {};
+    if (_.isArray(ids)) query._id = { $in: ids };
+    return User.find(query).populate('library.collections').limit(limit).exec()
+  },
+  collections: ({id}) => Collection.find({ _id: id }).exec()
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: loaders,
+  graphiql: true
+}));
+
 app.use('/', routes);
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
