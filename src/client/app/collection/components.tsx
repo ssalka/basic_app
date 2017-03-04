7ccb787e0a003a928b189a4e73b1f9aee2f6f757
Steven Salka <steven@ssalka.io>
2017-03-04 00:13:21 -0500

    fix indentation

--- src/client/app/collection/components.tsx
+++ src/client/app/collection/components.tsx
@@ -17,44 +17,44 @@ export default function getComponents(
 
   const SettingsButton = () => (
     <Link to={linkWithState(`${path}/edit`)}>
-    <Button icon="cog" minimal={true} size="small" />
+      <Button icon="cog" minimal={true} size="small" />
     </Link>
   );
 
   const AddDocumentButton = overrides => (
     <Link to={linkWithState(`${_location.pathname}/add`)}>
-    <Button icon="add" text={`Add ${_.singularize(name)}`}
-    color="primary"
-    minimal={true}
-    rounded={true}
-    {...overrides}
-    />
+      <Button icon="add" text={`Add ${_.singularize(name)}`}
+        color="primary"
+        minimal={true}
+        rounded={true}
+        {...overrides}
+      />
     </Link>
   );
 
   return {
     CollectionHeader: () => (
       <FlexRow alignItems="top">
-      <div className="collection-info">
-      <p><h3>{name}</h3> <h3 className="muted">({documents.length})</h3></p>
-      {description && <p>{description}</p>}
-      <AddDocumentButton size="small" />
-      <SettingsButton />
-      </div>
+        <div className="collection-info">
+          <p><h3>{name}</h3> <h3 className="muted">({documents.length})</h3></p>
+          {description && <p>{description}</p>}
+          <AddDocumentButton size="small" />
+          <SettingsButton />
+        </div>
       <Icon className="faded" name={icon} size={60} />
       </FlexRow>
     ),
     Placeholder: () => (
       <NonIdealState
-      visual={icon || 'document'}
-      title={`You don't have any ${name}`}
-      description={<span>All {name.toLowerCase()} you add will be visible here</span>}
-      action={<AddDocumentButton />}
+        visual={icon || 'document'}
+        title={`You don't have any ${name}`}
+        description={<span>All {name.toLowerCase()} you add will be visible here</span>}
+        action={<AddDocumentButton />}
       />
     ),
     Loading: () => (
       <FlexRow className="flex-view" justifyContent="center">
-      <div className="loader"></div>
+        <div className="loader"></div>
       </FlexRow>
     )
   };
