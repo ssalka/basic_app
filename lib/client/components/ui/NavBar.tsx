dda1b460a0f537e14736e13385b61b6de72c96ad
Steven Salka <steven@ssalka.io>
2017-03-12 16:49:06 -0400

    fix NavBar: make context-aware

--- lib/client/components/ui/NavBar.tsx
+++ lib/client/components/ui/NavBar.tsx
@@ -5,30 +5,41 @@ import { IContext, ReactElement } from 'lib/client/interfaces';
 import { BaseComponent, Button, Icon } from '../';
 import '../../styles/Navbar.less';
 
-export default (_, {appName, user}: IContext): ReactElement => (
-  <nav className="pt-navbar pt-dark">
-    <div className="pt-navbar-group pt-align-left">
-      <Link to={user ? '/home' : '/'} className="pt-navbar-heading">
-        {appName}
-      </Link>
-    </div>
-    <div className="pt-navbar-group pt-align-right">
-      {user ? (
-        <InputGroup
-          placeholder="Search"
-          className="pt-inputs pt-round"
-          leftIconName="search"
-          rightElement={<Button minimal={true} icon="cross" />}
-        />
-      ) : null}
-      <Link to={user ? '/logout' : '/login'}>
-        <Button
-          text={user ? null : 'Sign In'}
-          icon="user"
-          minimal={true}
-          rounded={true}
-        />
-      </Link>
-    </div>
-  </nav>
-);
+export default class NavBar extends BaseComponent<any, any> {
+  public static contextTypes = {
+    appName: React.PropTypes.string,
+    user: React.PropTypes.object
+  };
+
+  public render() {
+    const { appName, user }: IContext = this.context;
+
+    return (
+      <nav className="pt-navbar pt-dark">
+        <div className="pt-navbar-group pt-align-left">
+          <Link to={user ? '/home' : '/'} className="pt-navbar-heading">
+            {appName}
+          </Link>
+        </div>
+        <div className="pt-navbar-group pt-align-right">
+          {user ? (
+            <InputGroup
+              placeholder="Search"
+              className="pt-inputs pt-round"
+              leftIconName="search"
+              rightElement={<Button minimal={true} icon="cross" />}
+            />
+          ) : null}
+          <Link to={user ? '/logout' : '/login'}>
+            <Button
+              text={user ? null : 'Sign In'}
+              icon="user"
+              minimal={true}
+              rounded={true}
+            />
+          </Link>
+        </div>
+      </nav>
+    );
+  }
+}
