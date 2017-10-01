1de453b5d250cc55cfd2476d723bf97831933355
Steven Salka <steven@ssalka.io>
2017-09-30 22:31:47 -0400

    use material popover for collection icon selector

--- lib/client/components/ui/IconSelector.tsx
+++ lib/client/components/ui/IconSelector.tsx
@@ -1,62 +1,88 @@
 declare const _;
 declare const React;
-import { ViewComponent, Icon, Popover, FlexRow } from '../';
+
+import { findDOMNode } from 'react-dom';
+import Icon from 'material-ui/Icon';
+import Popover from 'material-ui/Popover';
+import { ViewComponent, FlexRow } from '../';
 import { IIcon, ReactElement, ReactProps } from 'lib/client/interfaces';
 import { ICONS } from 'lib/common/constants';
 import '../../styles/IconSelector.less';
 
 interface IProps extends ReactProps {
-  isOpen: boolean;
-  onClick: React.MouseEventHandler<any>;
   selectedIcon: string;
   onSelectIcon(iconId: string): void;
 }
 
-interface IState { icon: string; }
+interface IState {
+  anchorElement: any;
+  icon: string;
+  open: boolean;
+}
 
 export default class IconSelector extends ViewComponent<IProps, IState> {
-  public static defaultProps: Partial<IProps> = {
+  static defaultProps: Partial<IProps> = {
     selectedIcon: 'graph'
   };
 
+  selectedIconRef = null;
+
   constructor(props: IProps) {
     super(props);
-    this.state = { icon: props.selectedIcon };
+    this.state = {
+      anchorElement: null,
+      icon: props.selectedIcon,
+      open: false
+    };
   }
 
-  private SelectedIcon(): ReactElement {
-    return (
-      <Icon name={this.state.icon} onClick={this.props.onClick} />
-    );
-  }
+  handleRequestClose = () => this.setState({
+    open: false
+  });
 
-  private setIcon(icon: IIcon) {
+  handleTargetClick = () => this.setState({
+    open: true,
+    anchorElement: findDOMNode(this.selectedIconRef)
+  });
+
+  setIcon(icon: IIcon) {
     this.setState({ icon: icon.id });
     this.props.onSelectIcon(icon.id);
   }
 
-  private IconGrid({ name, icons }): ReactElement {
+  IconGrid({ name, icons }): ReactElement {
     const getIconSetter: (icon: IIcon) => React.MouseEventHandler<HTMLSpanElement> = (
-      (icon: IIcon) => () => this.setIcon(icon)
+      (icon: IIcon) => () => {
+        this.setIcon(icon);
+        this.handleRequestClose();
+      }
     );
 
     return (
-      <div key={name} className="icon-grid">
+      <div key={name} className="icon-grid" style={{ marginLeft: 20 }}>
         <h5>{_.capitalize(name)}</h5>
         <FlexRow justifyContent="flex-start" flexWrap={true}>
           {icons.map((icon: IIcon) => (
-            <Icon name={icon.id} onClick={getIconSetter(icon)} />
+            <Icon
+              className={`pt-icon pt-icon-${icon.id}`}
+              style={{ fontSize: 20, margin: 10 }}
+              onClick={getIconSetter(icon)}
+            />
           ))}
         </FlexRow>
       </div>
     );
   }
 
-  private IconGroups(): ReactElement {
+  setRef = node => {
+    this.selectedIconRef = node;
+  }
+
+  IconGroups(): ReactElement {
     const { IconGrid } = this;
 
     return (
-      <div className="scroll-y" style={{ maxHeight: '300px', maxWidth: '250px' }}>
+      <div className="scroll-y" style={{ maxHeight: 300, maxWidth: 250 }}>
         {_(ICONS)
           .groupBy('group')
           .map((icons: IIcon[], group: string): {} => ({ name: group, icons }))
@@ -67,21 +93,31 @@ export default class IconSelector extends ViewComponent<IProps, IState> {
     );
   }
 
-  public render() {
-    const {
-      SelectedIcon, IconGroups,
-      props: { isOpen }
-    } = this;
+  render() {
+    const { IconGroups } = this;
 
     return (
-      <Popover
-        target={<SelectedIcon />}
-        isOpen={isOpen}
-        position="bottom"
-      >
-        <h4>Select an Icon</h4>
-        <IconGroups />
-      </Popover>
+      <div>
+        <Icon
+          className={`pt-icon pt-icon-${this.state.icon}`}
+          style={{ fontSize: 20 }}
+          ref={this.setRef}
+          onClick={this.handleTargetClick}
+        />
+
+        <Popover
+          open={this.state.open}
+          anchorEl={this.state.anchorElement}
+          onRequestClose={this.handleRequestClose}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+          transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+        >
+          <h4 style={{ margin: 20 }}>
+            Select an Icon
+          </h4>
+          <IconGroups />
+        </Popover>
+      </div>
     );
   }
 }
