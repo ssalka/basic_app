0923724dcfa35ddd5119c5e264006f7f0db77f85
Steven Salka <steven@ssalka.io>
2017-10-14 21:17:44 -0400

    remove cartiv UserStore

--- lib/client/api/index.ts
+++ lib/client/api/index.ts
@@ -1,6 +1,5 @@
 import { createAPI } from 'cartiv';
+
 const api = createAPI();
-const { User } = api;
 
 export default api;
-export { User }; // BUG: User api is initially undefined; need to grab off of api object
--- src/client/app/home/index.tsx
+++ src/client/app/home/index.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import Link from 'react-router-redux-dom-link';
 import { NonIdealState } from '@blueprintjs/core';
-import { User } from 'lib/client/api';
 import {
   ReduxComponent,
   FlexRow,
