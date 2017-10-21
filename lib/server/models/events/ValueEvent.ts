b85bee812696a53e045b9f3fc14552d0bb15ce7a
Steven Salka <steven@ssalka.io>
2017-10-21 18:12:04 -0400

    add metadata field to ValueEvent model

--- lib/server/models/events/ValueEvent.ts
+++ lib/server/models/events/ValueEvent.ts
@@ -1,10 +1,19 @@
-import { ModelGen } from 'lib/server/utils';
+import { ModelGen, types } from 'lib/server/utils';
 import Event from './Event';
+const { ObjectId, Mixed, ref } = types;
 
 const ValueEventSchema = {
   value: {
-    type: Object,
+    type: Mixed,
     required: true
+  },
+  metadata: {
+    default: {},
+    type: {
+      collection: ref('Collection'),
+      document: ObjectId, // TODO: Document model (for user documents to extend)
+      field: ObjectId // TODO: Field model (to hold type information, names, and values)
+    }
   }
 };
 
