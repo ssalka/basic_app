2323e1816a46695ddc9713c63980c612bfb0ad5c
Steven Salka <steven@ssalka.io>
2017-10-28 21:24:43 -0400

    use tab to add current input value as an identifier

--- lib/client/components/ui/SmartInput.tsx
+++ lib/client/components/ui/SmartInput.tsx
@@ -6,19 +6,20 @@ import styled from 'styled-components';
 import { ViewComponent } from 'lib/client/components';
 import { Collection } from 'lib/common/interfaces';
 
+interface IIdentifier {
+  type: 'collection' | 'document';
+  value: string;
+  resolved?: any;
+}
+
 interface ISmartInputProps {
   initialWidth?: number;
   rowHeight?: number;
 }
 
 interface ISmartInputState {
-  rawValue: string;
-  matched: {
-    collection?: string;
-  };
-  resolved: {
-    collection?: Collection;
-  };
+  inputValue: string;
+  identifiers: IIdentifier[];
 }
 
 export default class SmartInput extends ViewComponent<
@@ -31,50 +32,106 @@ export default class SmartInput extends ViewComponent<
   };
 
   state: ISmartInputState = {
-    rawValue: null,
-    matched: {},
-    resolved: {}
+    inputValue: null,
+    identifiers: []
   };
 
-  handleChange(newValue: string) {
-    this.setState({ rawValue: newValue });
+  handleChange(event) {
+    this.setState({ inputValue: event.target.value });
+  }
+
+  handleKeyDown(event) {
+    if (isTabKeyEvent(event)) {
+      event.preventDefault();
+    }
+  }
+
+  handleKeyUp(event) {
+    if (isTabKeyEvent(event)) {
+      this.handleTab(event);
+    }
+  }
+
+  handleTab(event) {
+    if (_.isEmpty(this.state.inputValue)) return;
+
+    // TODO: need selected search suggestion to get type
+    const type = 'document';
+
+    this.setState({
+      inputValue: '',
+      identifiers: this.state.identifiers.concat({
+        type,
+        value: event.target.value
+      })
+    });
   }
 
   render() {
     const { initialWidth, rowHeight, ...props } = this.props;
-    const { rawValue, matched, resolved } = this.state;
+    const { identifiers, inputValue } = this.state;
 
     return (
       <StyledSmartInput
         {...props}
-        align="center"
-        justify="flex-start"
+        align="stretch"
         className="pt-card pt-elevation-1"
+        column={true}
         p={0}
-        px={2}
         width={initialWidth}
         style={{
-          height: rowHeight,
-          borderRadius: rowHeight / 2
+          borderRadius: rowHeight / 2,
+          minHeight: rowHeight
         }}
       >
-        {resolved.collection && <div>{resolved.collection.name}</div>}
+        {identifiers.map(({ value, resolved }, i) => (
+          <div
+            key={i}
+            children={resolved ? resolved.name : value}
+            style={{
+              height: rowHeight,
+              lineHeight: `${rowHeight}px`,
+              paddingLeft: 10
+            }}
+          />
+        ))}
 
-        <EditableText
+        <input
           placeholder="Search or Add to Library"
+          value={inputValue}
           onChange={this.handleChange}
-          value={rawValue}
+          onKeyDown={this.handleKeyDown}
+          onKeyUp={this.handleKeyUp}
+          style={{
+            marginLeft: 10
+          }}
         />
       </StyledSmartInput>
     );
   }
 }
 
+const isTabKeyEvent = (event): boolean => event.keyCode === 9;
+
 const StyledSmartInput: any = styled(Flex)`
   overflow: hidden;
 
-  .pt-editable-text {
+  * {
+    height: 32px;
+    line-height: ${({ style }: any) => style.minHeight}px:
+    margin-left: ${({ style }: any) => style.minHeight}px:
+  }
+
+  div {
+    background-color: #EEE;
+    margin-bottom: 0 !important;
+    border-bottom: 1px solid #CCC;c
+  }
+
+  input {
     flex-grow: 1;
+    border: 0;
+    font-size: 14px;
 
     &::before {
       box-shadow: none !important;
