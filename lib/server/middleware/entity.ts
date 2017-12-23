741d5e30e07b51b75d90e54bce766d273cd68113
Steven Salka <steven@ssalka.io>
2017-12-23 11:47:28 -0800

    add server-side updateEntity middleware

--- lib/server/middleware/entity.ts
+++ lib/server/middleware/entity.ts
@@ -18,6 +18,20 @@ export const createEntity: RequestHandler = (req, res, next) => {
     .catch(next);
 };
 
+export const updateEntity: RequestHandler = (req, res, next) => {
+  // TODO: add versioning to events, identify concurrent updates
+  EntityEvent.create({
+    type: EntityEventType.Updated,
+    creator: req.user._id,
+    payload: {
+      entityId: req.params.entityId,
+      updates: req.body
+    }
+  })
+    .then(entityEvent => res.json(entityEvent))
+    .catch(next);
+};
+
 export const getEntities: RequestHandler = (req, res, next) => {
   EntityEvent.project({
     type: EntityEventType.Created,
